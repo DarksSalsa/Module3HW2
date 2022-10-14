e2372569a44b871d816b32efa4fdb5fae248f848
Herman <germanriv19@gmail.com>
2022-10-14 19:37:08 +0300

    Slightly reshuffled project.

---
+++ CallList/CallBook/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace CallList
+{
+    public class Contact
+    {
+        public Contact(string fullName, string phoneNumber)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string FullName { get; init; }
+        public string PhoneNumber { get; init; }
+    }
+}
